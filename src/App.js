b4bfbbc282c67086caff84988c12696a8e4592bc
Nadeesha Abeywickrama <127606065+NadeeshaAbey@users.noreply.github.com>
2024-08-02 11:06:16 +0530

    create routs,navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs.js";
+import ContactUs from "./pages/ContactUs";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+      <Footer />
+    </div>
+    
+  );
 }
 
 export default App;
